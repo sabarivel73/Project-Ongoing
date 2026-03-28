3da0452d1f38109fb4bfbb2af57f62515e39aef8
sabarivel73 <sabarivel0928@gmail.com>
2026-03-28 15:46:32 +0530

    new changes

--- backend/src/main/java/code/backend/entity/displayChat.java
+++ backend/src/main/java/code/backend/entity/displayChat.java
@@ -1,7 +1,9 @@
 package code.backend.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
 
 @Data
@@ -9,9 +11,10 @@ public class displayChat {
     private Integer user_id;
     private String name;
     private Long readCount;
-    private LocalDateTime time;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Kolkata")
+    private Instant time;
 
-    public displayChat(Integer user_id, String name, Long readCount, LocalDateTime time) {
+    public displayChat(Integer user_id, String name, Long readCount, Instant time) {
         this.user_id = user_id;
         this.name = name;
         this.readCount = readCount;
